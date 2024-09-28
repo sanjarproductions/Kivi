bce9e3ede551df09734574eab35142a66600cae7
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2024-09-28 22:51:15 +0500

    Hero Section got started & made

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -4,16 +4,25 @@ import "./style.css"
 const H = () => {
     return (
         <>
-            <div className="homepg pg">
+            <div className="home">
                 <nav>
                     <div className="container">
+                        <a href='https://calendly.com/sanjar-coder/discovery-call' className=''>Book a Call</a>
 
                     </div>
                 </nav>
                 <main>
-                    <section>
+                    <section className='hero'>
                         <div className="container">
 
+                            <div className="hero-text">
+                                <h1 className='hero-title'>When Creativity <br /> Meets <span>Kivi</span></h1>
+                                <p className='hero-desc'>Discover a universe wheer <span>creativity flourishes</span>, turning ideas into <span>digital marvels</span> </p>
+                                <div>
+                                    <a href='https://calendly.com/sanjar-coder/discovery-call' className=''>Book a Call</a>
+                                    <a href='https://calendly.com/sanjar-coder/discovery-call'>Book a Call</a>
+                                </div>
+                            </div>
                         </div>
                     </section>
                     <section>
