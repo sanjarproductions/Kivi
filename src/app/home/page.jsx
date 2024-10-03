d195ab384631151a052ac73e31bcb22d97f2f73b
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2024-10-03 22:28:17 +0500

    slider made

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import Image from 'next/image'
+"use client"
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+// import required modules
+import { Autoplay,Pagination } from 'swiper/modules';
 
 import "./style.css"
+import Image from 'next/image'
 
 const H = () => {
-    
+
     return (
         <>
             <div className="home">
@@ -36,12 +45,38 @@ const H = () => {
                             </div>
                         </div>
                     </section>
-                    <section>
+                    <section className='our-work'>
                         <div className="container">
-
+                            <h2>OUR WORK</h2>
+                            <div className='our-work__text'>
+                                <p>Business challenges are tough, but we have a <span> proven record of elevating our </span> partners to their next and best selves</p>
+                                <button>See More Work</button>
+                            </div>
                         </div>
                     </section>
-                    <section>
+                    <section className='slider'>
+
+                        <Swiper
+                            slidesPerView={3}
+                            spaceBetween={30}
+                            loop={true}
+                            speed={1200}
+                            autoplay={{
+                                delay: 2500,
+                                disableOnInteraction: false,
+                            }}
+                            modules={[Autoplay,Pagination]} className="mySwiper">
+                            <SwiperSlide>Slide 1</SwiperSlide>
+                            <SwiperSlide>Slide 2</SwiperSlide>
+                            <SwiperSlide>Slide 3</SwiperSlide>
+                            <SwiperSlide>Slide 4</SwiperSlide>
+                            <SwiperSlide>Slide 5</SwiperSlide>
+                            <SwiperSlide>Slide 6</SwiperSlide>
+                            <SwiperSlide>Slide 7</SwiperSlide>
+                            <SwiperSlide>Slide 8</SwiperSlide>
+                            <SwiperSlide>Slide 9</SwiperSlide>
+                        </Swiper>
+
                         <div className="container">
 
                         </div>
