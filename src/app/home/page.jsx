8f55dcaefa1dbbc0a1f342abf7eb2cba667c5748
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2024-10-03 16:32:21 +0500

    buttons stlyes are complete

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -4,12 +4,14 @@ import Image from 'next/image'
 import "./style.css"
 
 const H = () => {
+    
     return (
         <>
             <div className="home">
+                <div className="blob" id='blob'></div>
                 <nav>
                     <div className="container">
-                        <a href='https://calendly.com/sanjar-coder/discovery-call' className='no-fill-btn' target='_blank'>Get in Touch</a>
+                        <a href='https://calendly.com/sanjar-coder/discovery-call' className='btn' target='_blank'>Get in Touch</a>
                         <button>
                             <img src="/img/burger-menu.svg" alt="Burger Menu" width={24} height={8} />
                         </button>
@@ -27,9 +29,9 @@ const H = () => {
                             <div className="hero-text">
                                 <h1 className='hero-title'>When Creativity <br /> Meets <span>Kivi</span></h1>
                                 <p className='hero-desc'>Discover a universe wheer <span>creativity flourishes</span>, turning ideas into <span>digital marvels</span> </p>
-                                <div>
-                                    <a href='https://calendly.com/sanjar-coder/discovery-call' className=''>Book a Call</a>
-                                    <a href='https://calendly.com/sanjar-coder/discovery-call'>Book a Call</a>
+                                <div className='btns-wrapper'>
+                                    <a className="booking__btn" href='https://calendly.com/sanjar-coder/discovery-call'>Book a Call</a>
+                                    <a className="refer__btn" href='https://calendly.com/sanjar-coder/discovery-call'>Refer a Friend</a>
                                 </div>
                             </div>
                         </div>
