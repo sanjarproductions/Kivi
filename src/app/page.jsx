6ebac95ccb692694ca473b3bb10282223d2fdd68
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2025-02-18 09:22:25 +0500

    .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,22 @@
-// "use client"
 import Waitlist from "./components/Waitlist/Waitlist";
 import "./globals.css"
 
 export default function Home() {
+
+  // may be
+  // setTimeout(() => {
+  //   console.log("Hello World")
+  // }, 1000)
+  
   return (
     <>
       <div className="waitlist-wrapper">
-        <h1 className='waitlist-title'>Join our the Waitlist</h1>
-        <p>& Be Part of Something Big</p>
+        <h1 className='waitlist-title'>Join our Waitlist</h1>
+        <p>& Become a Part of Something Big</p>
         <Waitlist />
       </div>
     </>
   );
 }
 
+//
